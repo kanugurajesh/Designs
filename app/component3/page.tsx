238eca1d9d986a38bf4750f85e1e7af5c1683a0a
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-16 09:34:07 +0530

    Adding the animated background mesh to the app

--- app/component3/page.tsx
+++ app/component3/page.tsx
@@ -1,19 +1,14 @@
-const page = () => {
+const Page = () => {
   return (
-    <main>
-      <div className="relative h-screen w-screen">
-        <div className="absolute bg-blue-200 h-full w-[50vw] clip-inclined right-0"></div>
-        <div className="flex">
-            <div>
-
-            </div>
-            <div>
-                
-            </div>
-        </div>
-      </div>
-    </main>
+    <div className="bg-gray-50 min-h-screen overflow-hidden relative">
+      {/* Blob 1 - Vivid Blue */}
+      <div className="absolute top-0 -left-10 w-[40rem] h-[40rem] bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-80 animate-blob"></div>
+      {/* Blob 2 - Vibrant Orange */}
+      <div className="absolute top-0 -right-10 w-[40rem] h-[40rem] bg-orange-400 rounded-full mix-blend-multiply filter blur-xl opacity-80 animate-blob animation-delay-2000"></div>
+      {/* Blob 3 - Bright Pink */}
+      <div className="absolute -bottom-16 left-1/4 w-[40rem] h-[40rem] bg-pink-500 rounded-full mix-blend-multiply filter blur-xl opacity-80 animate-blob animation-delay-4000"></div>
+    </div>
   );
 };
 
-export default page;
+export default Page;
